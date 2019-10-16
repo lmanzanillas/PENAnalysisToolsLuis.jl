23b35ff6e596c2cae68b0752f3dd3878b9e2f6b0
Felix Fischer <ffischer@mpp.mpg.de>
2019-10-16 12:01:15 +0200

    Added energy

--- src/ReadStruck.jl
+++ src/ReadStruck.jl
@@ -1,7 +1,7 @@
 """
-        `read_data_from_struck(filename:String; just_evt_t=false)`
+        read_data_from_struck(filename:String; just_evt_t=false)
 
-Reads one Struck (*.dat) file and returns a Named Table. 
+Reads one Struck (*.dat) file and returns a Named Table. Keys: samples, chid, evt_t, energy
 ...
 # Arguments
 - `filename::String`: Path to *.dat file as a string.
@@ -18,9 +18,10 @@ function read_data_from_struck(filename::String; just_evt_t=false)
         )
     else
         df = DataFrame(
-          evt_t   = Float64[],
-          samples = Array{Int32,1}[],
-          chid    = Int32[]
+            evt_t   = Float64[],
+            samples = Array{Int32,1}[],
+            chid    = Int32[],
+            energy  = []
         )
     end
 
@@ -30,12 +31,13 @@ function read_data_from_struck(filename::String; just_evt_t=false)
     for unsorted in reader
         nchunks += 1
         sorted = sortevents(unsorted)
+        #return sorted
         for evt in eachindex(sorted)
             ch = collect(keys(sorted[evt]))[1]
             if just_evt_t
                 push!(df, time(sorted[evt][ch]))
             else
-                push!(df, (time(sorted[evt][ch]), sorted[evt][ch].samples, sorted[evt][ch].chid))
+                push!(df, (time(sorted[evt][ch]), sorted[evt][ch].samples, sorted[evt][ch].chid, sorted[evt][ch].energy))
             end
 
         end
@@ -45,6 +47,6 @@ function read_data_from_struck(filename::String; just_evt_t=false)
     if just_evt_t
         return (evt_t = df.evt_t)
     else
-        return (evt_t = df.evt_t, samples = df.samples, chid = df.chid)
+        return (evt_t = df.evt_t, samples = df.samples, chid = df.chid, energy = df.energy)
     end
 end
