07f243e5d738ccd154743b8e8e9a108f61aeb170
Felix Fischer <ffischer@mpp.mpg.de>
2019-10-17 15:17:38 +0200

    Update take_struck_data.jl

--- src/Struck/take_struck_data.jl
+++ src/Struck/take_struck_data.jl
@@ -1,22 +1,52 @@
-function edit_pmt_daq(fadc_name, channel_nr; nsamples=512)
-    new_daq = open("/pmt_daq_backup.scala", "r") do file
-        temp = readlines(file)
-        i = 1
-        while i <= length(temp)
-            if length(split(temp[i], "val pmt1 = Ch(")) > 1
-                temp[i] = "val pmt1 = Ch("*string(channel_nr)*")"
-            elseif length(split(temp[i], "val adc = SIS3316(\"vme-sis3316://")) > 1
-                temp[i] = "val adc = SIS3316(\"vme-sis3316://"*fadc_name*"\", \"adc\")"
-            elseif length(split(temp[i], "val nSamples =  ")) > 1
-                temp[i] = "  val nSamples =  "*string(nsamples)
-            end
-            i +=1
-        end
-        return(temp)
+"""
+        take_struck_data(settings::NamedTuple)
+
+Creates an individual `pmt_daq.scala` file and takes data which are converted to a HDF5 file afterwards.
+...
+# Arguments
+- `settings::NamedTuple`: NamedTuple containing all settings. See Example.
+
+# Example settings
+- `settings = (fadc = "gelab-fadc08", 
+output_basename = "test-measurement", 
+data_dir = "../data/",
+conv_data_dir = "../conv_data/",
+measurement_time = 20,
+number_of_measurements = 5,
+channels = [1,2,3,4,5,6],
+trigger_threshold = [55],
+trigger_pmt = [5,6],
+peakTime = 2, 
+gapTime = 2, 
+nPreTrig = 192,
+nSamples = 256,
+saveEnergy = true
+) `
+...
+"""
+function take_struck_data(settings::NamedTuple)
+    if !isdir(settings.data_dir)
+        mkdir(settings.data_dir)
     end
-    open("pmt_daq.scala", "w") do file
-        for ln in new_daq
-            write(file, ln*"\n")
-        end    
+    if !isdir(settings.conv_data_dir)
+        mkdir(settings.conv_data_dir)
     end
+
+    create_struck_daq_file(settings)
+
+    current_dir = pwd()
+    cd(settings.data_dir)
+    i = 1
+    while i <= settings.number_of_measurements
+        run(`chmod -R 777 ./`)
+        run(`./pmt_daq.scala`);    
+        i += 1
+    end
+    rm("pmt_daq.scala")
+    cd(current_dir)
+    glob_str = settings.data_dir*"*"*settings.output_basename*"*.dat"
+    convert_dset_to_h5(glob_str, 
+        settings.output_basename, 
+        conv_data_dir=settings.conv_data_dir)
+    run(`chmod -R 777 ./`)
 end
