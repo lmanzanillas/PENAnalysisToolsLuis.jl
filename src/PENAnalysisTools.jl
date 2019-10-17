fe49b494ea2c4e7fa1201269b5bb8eb55ef83a15
Felix Fischer <ffischer@mpp.mpg.de>
2019-10-17 14:32:13 +0200

    Update PENAnalysisTools.jl

--- src/PENAnalysisTools.jl
+++ src/PENAnalysisTools.jl
@@ -13,10 +13,12 @@ using ProgressMeter
 using Pkg
 
 
-export convert_struck_to_h5, convert_dset_to_h5, read_data_from_struck
+export convert_struck_to_h5, convert_struck_to_h5, convert_dset_to_h5, read_data_from_struck, take_struck_data
 
 include("HDF5Conversion.jl")
 include("Struck/read_data_from_struck.jl")
+include("Struck/create_struck_daq_file.jl")
+include("Struck/take_struck_data.jl")
 
 
 end
