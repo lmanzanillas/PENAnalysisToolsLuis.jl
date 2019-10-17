d46cc0e8fa24df5334d912ee7a0ef3d67a57fa36
Felix Fischer <ffischer@mpp.mpg.de>
2019-10-17 15:20:46 +0200

    Update take_struck_data.jl

--- src/Struck/take_struck_data.jl
+++ src/Struck/take_struck_data.jl
@@ -7,21 +7,21 @@ Creates an individual `pmt_daq.scala` file and takes data which are converted to
 - `settings::NamedTuple`: NamedTuple containing all settings. See Example.
 
 # Example settings
-- `settings = (fadc = "gelab-fadc08", 
-output_basename = "test-measurement", 
-data_dir = "../data/",
-conv_data_dir = "../conv_data/",
-measurement_time = 20,
-number_of_measurements = 5,
-channels = [1,2,3,4,5,6],
-trigger_threshold = [55],
-trigger_pmt = [5,6],
-peakTime = 2, 
-gapTime = 2, 
-nPreTrig = 192,
-nSamples = 256,
-saveEnergy = true
-) `
+- `settings = (fadc = "gelab-fadc08",` 
+`output_basename = "test-measurement",`
+`data_dir = "../data/",`
+`conv_data_dir = "../conv_data/",`
+`measurement_time = 20,`
+`number_of_measurements = 5,`
+`channels = [1,2,3,4,5,6],`
+`trigger_threshold = [55],`
+`trigger_pmt = [5,6],`
+`peakTime = 2,`
+`gapTime = 2, `
+`nPreTrig = 192,`
+`nSamples = 256,`
+`saveEnergy = true`
+`) `
 ...
 """
 function take_struck_data(settings::NamedTuple)
