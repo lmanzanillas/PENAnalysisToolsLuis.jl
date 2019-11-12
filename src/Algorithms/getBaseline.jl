b4f785a536a1f7afa69fb83a67fa16bdfd48aa37
Luis Manzanillas <manzanil@pclg-04.mppmu.mpg.de>
2019-11-12 11:33:54 +0100

    fix peak finder

--- src/Algorithms/getBaseline.jl
+++ src/Algorithms/getBaseline.jl
@@ -10,7 +10,7 @@ removing +/- 5 samples around the peaks to avoid bias
 function getBaseline(signal::Vector)
     Baseline = copy(signal)
     peaks_threshold = mean(Baseline) + 15.0 # only peaks with amplitudes 15 units larger than the average  
-    peak_pos = findlocalmaxima(Baseline,peaks_threshold)
+    peak_pos = findLocalMaxima(Baseline,peaks_threshold)
     #peak_pos = findall(x -> x > peaks_threshold, signal)
     index_to_delete = []
     for i in peak_pos
