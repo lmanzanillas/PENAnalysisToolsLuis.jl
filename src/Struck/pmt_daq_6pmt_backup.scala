08cd6808c5ef9feebff6a6a86c9b49db18f64771
Luis Manzanillas <manzanil@pclg-04.mppmu.mpg.de>
2020-01-29 09:55:00 +0100

    update scala file

--- src/Struck/pmt_daq_6pmt_backup.scala
+++ src/Struck/pmt_daq_6pmt_backup.scala
@@ -46,6 +46,7 @@ val all_pmts = Ch(pmt_for_trigger) ++ other_pmts
 
 def configureADC_hpge(): Unit = {
   adc.trigger_intern_gen_set(pmt_for_trigger --> true)
+  adc.trigger_intern_gen_set(other_pmts --> true)
   adc.trigger_intern_feedback_set(pmt_for_trigger-->true)
   adc.trigger_extern_enabled_set(all_pmts --> true)
 
@@ -54,7 +55,7 @@ def configureADC_hpge(): Unit = {
 
   adc.trigger_gate_window_length_set(all_pmts --> 10)
 
-  adc.trigger_threshold_set(all_pmts --> 50)
+  adc.trigger_threshold_set(all_pmts --> 30)
   adc.trigger_cfd_set(all_pmts --> CfdCtrl.CDF50Percent)
   adc.trigger_peakTime_set(all_pmts --> 4)
   adc.trigger_gapTime_set(all_pmts --> 4)
